c53411d84f1f378e80bff4ba527d57cfaafe1f9a
docx68 <docx68@msn.com>
2021-12-21 14:45:32 +0800

    重构

--- src/controller/user.ctl.js
+++ src/controller/user.ctl.js
@@ -1,17 +1,24 @@
-import User from "../model/user.mdl.js";
+import {UserHandle} from "../model/user.mdl.js";
 
 
 class UserController {
-    add = async (ctx,next)=>{
+    signin = async (ctx,next)=>{
         const {user_name,password,is_amdmin} = ctx.request.body;
-        const res = await User.create({user_name,password,is_amdmin})
-        ctx.body = {
-            code:0,
-            message:'用户注册成功',
-            result:{
-                id:res.id,
-                user_name:res.user_name
+
+        let userHandle = new UserHandle();
+        try {
+            const res = await userHandle.createUser({user_name,password,is_amdmin})
+            ctx.body = {
+                code:0,
+                message:'用户注册成功',
+                result:{
+                    id:res.id,
+                    user_name:res.user_name
+                }
             }
+        } catch (err) {
+            console.log(err)
+            
         }
         //let userMDL = new User()
         // try{

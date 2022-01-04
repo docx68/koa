1299d71de62d5865e777c0560d17b0034a471c47
docx68 <docx68@msn.com>
2022-01-04 10:49:09 +0800

    文件上传koabody配置

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,4 @@
+import path  from 'path';
 import Koa from 'koa'
 import koaBody from 'koa-body'
 import router from '../routes/index.js';
@@ -5,7 +6,13 @@ import error from '../common/err.js';
 
 const app = new Koa()
 
-app.use(koaBody());
+app.use(koaBody({
+    multipart:true,
+    formidable:{
+        uploadDir:path.join('.',"src/uploads/"),
+        keepExtensions:true 
+    }
+}));
 app.use(router.routes()).use(router.allowedMethods())
 app.on('error', error);
 

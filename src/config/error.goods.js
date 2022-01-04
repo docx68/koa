5b0f644c82419695686568e7ad7eecd56e15012d
docx68 <docx68@msn.com>
2022-01-04 15:18:14 +0800

    商品错误配置处理

--- src/config/error.goods.js
+++ src/config/error.goods.js
@@ -6,5 +6,15 @@ export default {
             message:'文件上传失败',
             result:""
         }
+    },
+
+    // 上传不支持的文件格式
+    unSupportedType:{
+        status:402,
+        body:{
+            code:10102,
+            message:'不支持的文件格式',
+            result:""
+        }
     }
 }

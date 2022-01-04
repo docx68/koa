cd221837c2c3d4114bfd599d5023c4fc9e082e02
docx68 <docx68@msn.com>
2022-01-04 13:54:08 +0800

    商品模块控制器

--- src/controller/goods.controller.js
+++ src/controller/goods.controller.js
@@ -1,6 +1,22 @@
-class GoodsController {
-    uploads = async (ctx,next) => {
+import path  from "path";
+import errorGoods from "../config/error.goods.js";
 
+class GoodsController {
+    // 文件上传处理
+    uploads = async (ctx,next) => {
+        let file = ctx.request.files.file;
+        if (file) {
+            ctx.body = {
+                code:0,
+                message:'文件上传成功',
+                result:{
+                    file_name:path.basename(file.path)
+                }
+            }
+        } else {
+            ctx.app.emit('error',errorGoods.uploadsError,ctx);
+            return;
+        }
     }
 }
 

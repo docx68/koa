7198c07295b6f0610d3f72c1e822b2fa1584b65e
docx68 <docx68@msn.com>
2022-01-05 20:01:58 +0800

    数据库连接

--- src/db/config.js
+++ src/db/config.js
@@ -1,13 +1,22 @@
-//数据库连接，使用sequelize
-
+// 数据库连接，使用sequelize
 import Sequelize from 'sequelize';
-import { DB_CONFIG as db } from '../app/config.js';
 
+// 导入数据库配置文件
+import { DB_CONFIG as db } from '../app/config.js';
 let db_prefix = db.prefix
 
+// 传递参数实例化连接
 const db_connect = new Sequelize(db.database,db.username,db.password,{
     host:db.DB_HOST,
     dialect: db.dialect
 })
 
+// 测试连接是否成功
+try {
+    await db_connect.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+// 导出
 export { db_connect,db_prefix }

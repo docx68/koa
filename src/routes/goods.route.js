70ff0eeb78a658b262a7fedade697c09f33baeaa
docx68 <docx68@msn.com>
2022-01-05 15:53:03 +0800

    商品路由

--- src/routes/goods.route.js
+++ src/routes/goods.route.js
@@ -2,16 +2,24 @@ import Router from "koa-router";
 
 import AuthMiddleware from "../middleware/auth.middleware.js";
 import GoodsController from "../controller/goods.controller.js";
+import GoodsValidator from '../validator/goods.validator.js'
 
 const router = Router({prefix:'/goods'});
 
 const authMiddleware =new AuthMiddleware();
 const goodsController = new GoodsController()
+const goodsValidator = new GoodsValidator();
 
 router.post('/uploads',
     authMiddleware.auth,
     authMiddleware.isAdmin,
     goodsController.uploads
+)
+
+router.post('/add',
+
+    goodsValidator.validator,
+    goodsController.add
 )
 
 export default router;
